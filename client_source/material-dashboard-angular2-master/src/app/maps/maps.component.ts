c349eeaaa12a8e44ed0bed76839ffcca9b2c3955
shmuelb <shmuel.blizinsky@mail.huji.ac.il>
2023-10-10 14:34:27 +0300

    Improves2

--- client_source/material-dashboard-angular2-master/src/app/maps/maps.component.ts
+++ client_source/material-dashboard-angular2-master/src/app/maps/maps.component.ts
@@ -3,7 +3,7 @@ import {MapService} from './map.service';
 import {HttpService} from '../http/http.service';
 import {catchError, map, Observable, Subject, takeUntil, tap, throwError} from 'rxjs';
 import {HttpErrorResponse} from '@angular/common/http';
-import {MapEvent} from '../data/types';
+import {Coordinate, MapEvent, MapEventTypeEnum} from '../data/types';
 import {AppService} from '../app/app.service';
 
 declare const google: any;
@@ -24,13 +24,21 @@ export class MapsComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.mapService.initMap();
+        ///
+        const demo: MapEvent = {
+            id: '9823',
+            description: 'אירוע נפילת רקטה',
+            type: MapEventTypeEnum.missile,
+            coordinate: new Coordinate(31.540895, 34.580934)
+        };
+        this.mapService.addEventMarker(demo);
+        ///
         this.appService.mapEvents$
             .pipe(takeUntil(this._unsubscribeAll))
             .subscribe((mapEvents: MapEvent[]) => {
                 this.mapEvents = mapEvents;
                 // Todo: Check for multiplication
-                this.mapEvents.forEach((event: MapEvent) => {
-                    this.mapService.addEventMarker(event);
+                this.mapEvents?.forEach((event: MapEvent) => {
                 });
                 this._changeDetectorRef.markForCheck();
             });
