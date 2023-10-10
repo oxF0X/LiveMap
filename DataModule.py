e0a62f7e7e7bba851f9406236247dfdb7c91685f
Adam Liberov <adamsholet@gmail.com>
2023-10-10 23:16:23 +0300

    a

--- DataModule.py
+++ DataModule.py
@@ -1,16 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-class dataMoudle:
-    def __innit__(self):
+
+class dataModule:
+    def __init__(self):
         self
 
     def Scraping(self):
-        data = {
-
-        }
+        data = {}
         url = "https://hamal.co.il/main"
-
         try:
             response = requests.get(url)
 
@@ -18,15 +16,17 @@ class dataMoudle:
                 soup = BeautifulSoup(response.text, 'html.parser')
 
             else:
-                print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+                print(
+                    f"Failed to retrieve the webpage. Status code: {response.status_code}")
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {str(e)}")
 
-        articlesSoup = soup.find_all(attrs={'class':'styles_article__Mwzjl'})
+        articlesSoup = soup.find_all(attrs={'class': 'styles_article__Mwzjl'})
         articles = []
         linkComponents = []
         for article in articlesSoup:
-            linkComponent = article.find(attrs={'class':'styles_bodyContainer__w4Y99'})
+            linkComponent = article.find(
+                attrs={'class': 'styles_bodyContainer__w4Y99'})
             linkComponents.append(linkComponent)
 
         links = []
@@ -36,13 +36,8 @@ class dataMoudle:
             linkString = str(actualLinkComponent)
             linkString = linkString[9:-6]
             links.append(linkString)
-
-        print(links)
-
-
         for link in links:
             url = f"https://hamal.co.il{link}"
-            print(url)
 
             try:
                 response = requests.get(url)
@@ -52,17 +47,16 @@ class dataMoudle:
 
                     # print(soup.prettify())
                 else:
-                    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+                    print(
+                        f"Failed to retrieve the webpage. Status code: {response.status_code}")
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred: {str(e)}")
 
-
-            title = str(soup.find(attrs={"class":"styles_title__QOrfi"}).get_text())
-            content = str(soup.find(attrs={"class":"styles_action__8YDU4 styles_mail__6E83G"}))
+            title = str(
+                soup.find(attrs={"class": "styles_title__QOrfi"}).get_text())
+            content = str(
+                soup.find(attrs={"class": "styles_action__8YDU4 styles_mail__6E83G"}))
 
             data[title] = content
 
-
         return data
-
-
